9bc26ee88fd37eea6be09602c99d0315f204c5d2
Bryan Poh <zandrex00@gmail.com>
2020-06-05 11:55:07 +0800

    Initial commit

---
+++ app/src/main/java/com/bryanpoh/c347_p07_demofragment/FragmentSecond.java
@@ -0,0 +1,41 @@
+package com.bryanpoh.c347_p07_demofragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentSecond#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentSecond extends Fragment {
+
+    Button btnAddText;
+    TextView tvFrag2;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_second, container,false);
+        tvFrag2 = view.findViewById(R.id.tvFrag2);
+        btnAddText = view.findViewById(R.id.btnAddTextFrag2);
+
+        btnAddText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = tvFrag2.getText().toString() + "\n" + "New Data F2";
+                tvFrag2.setText(data);
+            }
+        });
+
+        return view;
+    }
+}
